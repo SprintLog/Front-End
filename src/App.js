3be21674f73dc4c8d70c323940b883a870eae4b3
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-12-15 23:25:11 +0700

    :tada: :tada: install Laravel :sparkles:

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <p>aasasas</p>
-)
-export default App;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
-
-const jsx = (
-  <h1>ddfdf</h1>
-)
-
-ReactDOM.render(jsx, document.getElementById('root'));
