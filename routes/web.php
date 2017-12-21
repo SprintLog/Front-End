6cdf8861cbccb4a856a8113644112fae522f3310
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-12-21 20:31:00 +0700

    :green_apple: Route For Test

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 /* --------------- Front End -------------------- */
 
 Route::get('/home', 'viewController@pageHome');
+Route::get('/projectinfo', 'viewController@pageprojectInfo');
