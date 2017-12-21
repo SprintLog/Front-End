00137dc62b913cefab53d474d60c293051434223
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-12-21 14:39:40 +0700

    layout fix :chart_with_upwards_trend:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.template')
-
 @section('content')
+  <div class="jumbotron">
+    <h1>WelCome Sprint Log</h1>
+
+      <p>
+        <a class="btn btn-lg btn-primary" href="#" role="button">
+          Fucking &raquo;
+        </a>
+      </p>
+  </div>
+
+
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,49 +3,48 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{elixir('css/app.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css">
+    <script src="js/app.js"></script>
+    <link rel="stylesheet" href="{{elixir('css/style.css')}}">
 
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
     <title>﻿Ｓ Ｌ</title>
   </head>
   <body>
-    <!-- Fixed navbar -->
-     <nav class="navbar navbar-default navbar-fixed-top">
-       <div class="container">
-         <div class="navbar-header">
-           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-             <span class="sr-only">Toggle navigation</span>
-             <span class="icon-bar"></span>
-             <span class="icon-bar"></span>
-             <span class="icon-bar"></span>
-           </button>
-           <a class="navbar-brand" href="#">﻿SpintLog</a>
-         </div>
-         <div id="navbar" class="navbar-collapse collapse">
-           <ul class="nav navbar-nav">
-             <li class="active"><a href="#">Home</a></li>
-             <li><a href="#">Project Info</a></li>
-             <li><a href="#">Planing</a></li>
-             <li><a href="#">Estimage</a></li>
-             <li><a href="#">Kanban Board</a></li>
-             <li><a href="#">Upload</a></li>
-             <li><a href="#about">About</a></li>
-             <li><a href="#contact">Contact</a></li>
-           </ul>
-           <ul class="nav navbar-nav navbar-right">
-             <li><a href="../navbar/">Default</a></li>
-             <li><a href="../navbar-static-top/">Static top</a></li>
-             <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
-           </ul>
-         </div><!--/.nav-collapse -->
-       </div>
-     </nav>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-              @yield('content')
+    <div class="container">
+
+      <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">﻿Ｓ Ｌ</a>
           </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Project Info</a></li>
+              <li><a href="#">Planing</a></li>
+              <li><a href="#">Estimage</a></li>
+              <li><a href="#">Kanban Board</a></li>
+              <li><a href="#">Upload</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">User</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+      <div class="row">
+        <div class="col-md-offset-1 col-md-10">
+               @yield('content')
+        </div>
       </div>
-    </div>
+    </div> <!-- /container -->
+
   </body>
 </html>
