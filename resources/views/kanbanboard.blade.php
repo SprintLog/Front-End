9d243ca3fc9c64071b76469a81d10b8d0a48f951
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-12-25 22:19:23 +0700

    :rocket: :rocket::rocket:confix color

--- resources/views/kanbanboard.blade.php
+++ resources/views/kanbanboard.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.template')
 @section('style')
   <link rel="stylesheet" href="css/jkanban.min.css">
-
-
+  <style media="screen">
+     .success{background: #00B961;}
+     .info{background: #2A92BF;}
+     .warning{background: #F4CE46;}
+     .error{background: #FB7D44;}
+  </style>
 @endsection
 @section('script')
 
@@ -10,5 +14,10 @@
 
 @section('content')
   <div id="myKanban"></div>
+
+  <button id="addDefault">Add "Default" board</button><br>
+   <button id="addToDo">Add element in "To Do" Board</button><br>
+   <button id="removeBoard">Remove "Done" Board</button><br>
+   <button id="removeElement">Remove "My Task Test"</button>
 
 @endsection
