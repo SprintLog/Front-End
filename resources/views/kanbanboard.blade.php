e6bad49f732374bc3ab59d3d88f87e71479d0f40
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-12-26 15:22:09 +0700

    :green_apple: fix kanban 50 % :pushpin::pushpin:

--- resources/views/kanbanboard.blade.php
+++ resources/views/kanbanboard.blade.php
@@ -3,22 +3,27 @@
   <link rel="stylesheet" href="/css/EJ-kanban.css">
 @endsection
 
+@section('script')
+@endsection
+
 @section('content')
   <div class="jumbotron far">
-    <header class="header">
-      <p class="total-card-counter" id="totalCards"></p>
-    </header>
     <div class="form-group row far">
-      <form id="frmAddTodo" class="form-add-todo">
-        Add Project:
-        <input type="text" autocomplete="off" name="todo_text" id="" value="" placeholder="Write and press enter" />
-      </form>
+        <p class="total-card-counter" id="totalCards"></p>
+        <form id="frmAddTodo" class="form-add-todo">
+          Add Project:
+          <input type="text" autocomplete="off" name="todo_text" id="" value="" placeholder="Write and press enter" />
+        </form>
     </div>
-
-    <div class="boards" id="boards"></div>
-    <form id="frmAddList" class="form-add-list">
-      Add List:
-      <input type="text" autocomplete="off" name="list_name" id="" value="" placeholder="List name" />
-    </form>
+    <div class="form-group row far">
+      <div class="boards" id="boards"></div>
+    </div>
   </div>
 @endsection
+
+{{--   ADD LIST
+  <form id="frmAddList" class="form-add-list">
+  Add List:
+    <input type="text" autocomplete="off" name="list_name" id="" value="" placeholder="List name" />
+  </form>
+--}}
