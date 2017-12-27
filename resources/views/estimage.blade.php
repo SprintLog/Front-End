9becc76e66e302d2c0b0b29eb1100256f2d8ab6e
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-12-27 17:12:01 +0700

    :globe_with_meridians: :globe_with_meridians: modal

--- resources/views/estimage.blade.php
+++ resources/views/estimage.blade.php
@@ -10,11 +10,11 @@
   <div class="jumbotron far">
     <div class="row far">
       <div class="col-sm-offset-2 col-sm-8">
-        <label  class="col-sm-3 col-form-label label label-default">
+        <label  class="col-sm-4 col-form-label label label-default">
             Calculate UUCPs
         </label>
 
-        <table class="table table-striped">
+        <table class="table table-striped ">
           <thead>
             <tr>
               <th scope="col">Complexity</th>
@@ -41,7 +41,7 @@
 
     <div class="row far">
       <div class="col-sm-offset-2 col-sm-8">
-        <label  class="col-sm-5 col-form-label label label-default">
+        <label  class="col-sm-6 col-form-label label label-default">
             Calculate Technical Complexity
         </label>
         <table class="table table-striped">
@@ -52,44 +52,74 @@
             </tr>
           </thead>
           <tbody>
+            @php
+              $TC = array("Distributed system",
+                      "Performance objectives",
+                      "End-user efficiency",
+                      "Complex processing",
+                      "Reusable code",
+                      "Easy to install",
+                      "Easy to use",
+                      "Portable",
+                      "Easy to change",
+                      "Concurrent use",
+                      "Security",
+                      "Access for third parties",
+                      "Training needs");
+            @endphp
             @for ($i=0; $i < 10; $i++)
               <tr>
-                <th scope="row">Distributed system</th>
+                <th scope="row">{{$TC[$i]}}</th>
                 <td>
-                  <div class="col-sm-5">
+                  <div class="col-sm-7">
                     <input type="number" class="form-control" min="1" max="5" >
                   </div>
                 </td>
               </tr>
             @endfor
-            <tr>
-              <th scope="row">Performance objectives</th>
-              <td>
-                <div class="col-sm-5">
-                  <input type="number" class="form-control" min="1" max="5" >
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">End-user efficiency</th>
-              <td>
-                <div class="col-sm-5">
-                  <input type="number" class="form-control" min="1" max="5" >
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Distributed system</th>
-              <td>
-                <div class="col-sm-5">
-                  <input type="number" class="form-control" min="1" max="5" >
-                </div>
-              </td>
-            </tr>
           </tbody>
         </table>
+        <label  class="col-sm-6 col-form-label label label-default">
+            Calculate Technical Complexity
+        </label>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Factor</th>
+              <th scope="col">&nbsp;&nbsp;&nbsp;Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            @php
+              $EF = array("Familiar with the development process",
+                      "Application experience",
+                      "Object-oriented experience",
+                      "Lead analyst capability",
+                      "Motivation",
+                      "Stable requirements",
+                      "Part-time staff",
+                      "Difficult programming language");
+            @endphp
+            @for ($i=0; $i < 8; $i++)
+              <tr>
+                <th scope="row">{{$EF[$i]}}</th>
+                <td>
+                  <div class="col-sm-10">
+                    <input type="number" class="form-control" min="1" max="5" >
+                  </div>
+                </td>
+              </tr>
+            @endfor
+          </tbody>
+        </table>
+        <div class="row">
+          <div class="col-sm-offset-4 col-sm-8">
+            <button type="button" name="button" class="btn">Cancle</button>
+            <button type="button" name="button" class="btn">Save</button>
+          </div>
+        </div>
       </div>
-    <div>
+    </div>
 
 
 
