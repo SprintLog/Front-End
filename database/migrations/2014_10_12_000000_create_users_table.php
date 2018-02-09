1dd6c04550804d861dc1ae7eea51db0bd2e05c41
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-02-09 12:25:36 +0700

    ADD atrbite on table Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('projectId');
+            $table->string('typeUser');
             $table->rememberToken();
             $table->timestamps();
         });
