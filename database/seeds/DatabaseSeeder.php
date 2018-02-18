691abea9a235ff9987c47805916559fb302fab77
Sarawut_064 <mamochiro11@gmail.com>
2018-02-18 23:10:44 +0700

    Test Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      $this->call([
+      UsersSeeder::class,
+      TypeProjectSeeder::class,
+      //ProjectsSeeder::class,
+
+  ]);
     }
 }
