61de73a96ad06ec35d9bf300dbc853b99c724db0
Sutita_009 <s5730213009@phuket.psu.ac.th>
2018-02-19 10:19:06 +0700

    add authcontroll

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Validator;
+use Auth;
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+
+      $email = User::where('email','=',$request->email)->value('email');
+      var_dump($email);
+      $password = User::where('password','=',bcrypt($request->password));
+      if (is_null($email) &&  is_null($password)){
+
+      }
+          // if (Auth::attempt(['email' => $request->email, 'password' => bcrypt($request->password)])){
+          //
+          // }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      return view('auth.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      // dd($request);
+
+      $validator = Validator::make($request->all(),[
+           'name'        => 'required|nullable|string|max:25',
+           'lastname'    => 'required|nullable|string|max:25',
+           'email'       => 'required|nullable|unique:users|max:150',
+           'password'    => 'required|nullable|max:25',
+           'typeuser'    => 'required|numeric'
+       ]);
+
+
+
+       if ($validator->fails()) {
+           return back()->withErrors($validator)->withInput();
+       }
+
+      $user = new User;
+      $user->projectid  = 0; // zero ก่อนเพราะยังไม่ได้ใส่ค่าตอนสร้างโปรเจค
+      $user->name       = $request->name;
+      $user->lastname   = $request->lastname;
+      $user->email      = $request->email;
+      $user->password   = bcrypt($request->password);
+      $user->typeuser   = $request->typeuser;
+
+      $user->save();
+      return back()->with('success', 'Register Success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
