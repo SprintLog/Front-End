b5db5a5d4dfe708d7fcda68932bc6d7e2c20a0be
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-02-25 03:13:46 +0700

    :bento: :boom: Confix Seeder :boom: :boom:

--- database/migrations/2018_02_06_154427_create_ecfs_table.php
+++ database/migrations/2018_02_06_154427_create_ecfs_table.php
@@ -15,6 +15,12 @@ class CreateEcfsTable extends Migration
     {
         Schema::create('ecfs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('topic');
+            $table->string('des');
+            $table->integer('weight');
+            $table->integer('rate');
+            $table->integer('result');
+            $table->integer('projectId');
             $table->timestamps();
         });
     }
