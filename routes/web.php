2d1c82ba2b35051a6b5fb6c87840a4a248c1b378
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-02-27 17:53:12 +0700

    REFACETORING CODE  :recycle: :recycle: :recycle:

--- routes/web.php
+++ routes/web.php
@@ -20,28 +20,19 @@ Route::resource('/project', 'ProjectController');
 
 Route::resource('/projectcreate', 'ProjectListController');
 
-
+Route::resource('/estimage', 'EffortEstimationsController')->name('tcf','ecf');
+// Upadete All Only
+Route::post('/estimage_updateall', 'EffortEstimationsController@updateAll');
 
 /* --------------- VIEW ONLY -------------------- */
+
+
 
 Route::get('/listproject', 'viewController@pageListProject');
 Route::get('/homet', 'viewController@pageHome');
 Route::get('/projectinfo', 'viewController@pageprojectInfo');
 Route::get('/planing', 'viewController@pagePlaning');
-Route::get('/estimage', 'viewController@pageEstimage');
+
 Route::get('/kanbanBoard', 'viewController@pageKanbanBoard');
 Route::get('/upload', 'viewController@pageUpload');
 Route::get('/dashboard', 'viewController@pageDashboard');
-<<<<<<< HEAD
-=======
-Route::get('/pageListProject', 'viewController@pageListProject');
-Route::get('/projectlist', 'viewController@projectlist');
-
-Route::resource('/projectinfo', 'ProjectController');
-Route::get('/projectlist', 'projectController@show')->name('project');
-Route::post('/projectlist_delete', 'ProjectController@delete');
-
-//estmage
-Route::get('/estimage', 'TcfController@show')->name('tcf','ecf');
-Route::post('/estimage_update', 'TcfController@update');
->>>>>>> Mark
