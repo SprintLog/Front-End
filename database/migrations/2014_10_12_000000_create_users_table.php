ba6eaf3888c2cce759e2c35554dcebbd59f2c530
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-03-01 13:32:45 +0700

    :construction: :construction: :construction: :construction: :construction:

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,13 +19,7 @@ class CreateUsersTable extends Migration
          $table->string('lastname');
          $table->string('email')->unique();
          $table->string('password');
-
-         //$table->integer('projectId')->unsigned();
-         //$table->string('typeUser');
-
-         $table->integer('projectid')->nullable();
          $table->integer('typeUser')->nullable();
-
          $table->rememberToken();
          $table->timestamps();
         });
--- database/migrations/2018_02_10_034414_create_type_project_table.php
+++ database/migrations/2018_02_10_034414_create_type_project_table.php
@@ -15,30 +15,29 @@ class CreateTypeProjectTable extends Migration
     {
         Schema::create('type_project', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('type');
             $table->timestamps();
         });
 
-/*
-        Schema::table('users', function(Blueprint $table){
+
+
+        Schema::table('macths', function(Blueprint $table){
+            $table->foreign('userId')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
             $table->foreign('projectId')
                 ->references('id')->on('projects')
                 ->onDelete('cascade');
 
-        });*/
-
-
-
+        })
 
         Schema::table('projects', function(Blueprint $table){
             $table->foreign('typeProjectId')
                 ->references('id')->on('type_project')
                 ->onDelete('cascade');
-
-            $table->foreign('userId')
-                ->references('id')->on('users')
-                ->onDelete('cascade');
-
         });
+        
         Schema::table('tasks', function(Blueprint $table){
             $table->foreign('projectId')
                 ->references('id')->on('projects')
