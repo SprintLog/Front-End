7a8fb3ab699e9176f97001a5329202f511e6f76b
Sarawut_064 <mamochiro11@gmail.com>
2018-03-01 21:55:45 +0700

    Add Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,20 +1,46 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Task;
+use Illuminate\Support\Facades\Validator;
+
 class TaskController extends Controller
 {
+    public function index()
+    {
+      $tasks = DB::table('tasks')->get();
+      return view('addTask', ['tasks' => $tasks]);
+    }
+
     public function insert(Request $request){
+      $validator = Validator::make($request->all(), [
+            'nameTask' => 'required|max:255',
+            'complexity' => 'required|max:255',
+            'projectId' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator)
+                ->with('warning', 'plz check input');
+        }
+
       // dd($request);
       $task = new Task;
-      $task->nametask   = $request->nametask;
+      $task->nametask   = $request->nameTask;
       $task->complexity = $request->complexity;
       // $task->projectId  = $request->projectId; WAIT MAKE PROJECT & USER
-      $task->projectId  = 1;
+      $task->projectId  = $request->projectId;
       $task->save();
-      echo "sucess";
+      return back();
+    }
+
+    public function destroy($id)
+    {
+      $task = Task::find($id)->delete();
       return back();
     }
 }
