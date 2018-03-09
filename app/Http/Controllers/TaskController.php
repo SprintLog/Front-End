7f02b857dd7eb1cd57e381f6edd59723cd69eed2
Sarawut_064 <mamochiro11@gmail.com>
2018-03-09 23:11:21 +0700

    Fix Addtask

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $validator = Validator::make($request->all(), [
+            'nameTask' => 'required|max:255',
+            'complexity' => 'required|max:255',
+            'projectId' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator)
+                ->with('warning', 'plz check input');
+        }
+
+      // dd($request);
+      $task = new Task;
+      $task->nametask   = $request->nameTask;
+      $task->complexity = $request->complexity;
+      $task->projectId  = $request->projectId;
+      $task->save();
+      return back();
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $globalsID=$id;
+      $tasks = DB::table('tasks')->where('projectId','=',$id)->get();
+      // dd($tasks);
+      return view('addTask', ['tasks' => $tasks]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $task = Task::find($id)->delete();
+      return back();
+    }
+}
--- resources/views/addTask.blade.php
+++ resources/views/addTask.blade.php
@@ -17,7 +17,7 @@
 
 
                       <!-- New Task Form -->
-                      <form action="{{ url('task')}}" method="POST" class="form-horizontal">
+                      <form action="{{ url('/task/1')}}" method="POST" class="form-horizontal">
                           {{ csrf_field() }}
 
                           <!-- Task Name -->
@@ -36,8 +36,8 @@
                                 </select>
                               </div>
                             </div>
-                    
-                              <input type="hidden" name="projectId" value="3">
+
+                              {{-- <input type="hidden" name="projectId" value="{{ }}"> --}}
                           </div>
                           <!-- Add Task Button -->
                           <div class="form-group">
@@ -62,13 +62,18 @@
                           <table class="table table-striped task-table">
                               <thead>
                                   <th>Task</th>
+                                  <th>Complexity</th>
                                   <th>&nbsp;</th>
                               </thead>
                               <tbody>
                                   @foreach ($tasks as $tasks)
                                       <tr>
                                           <td class="table-text"><div>{{ $tasks->nametask }}</div></td>
-
+                                          <td class="table-text"><div>@if($tasks->complexity == 1) Simple
+                                          @elseif ($tasks->complexity == 2) Middle
+                                          @else Complex
+                                          @endif
+                                          </div></td>
                                           <!-- Task Delete Button -->
                                           <td>
                                               <form action="{{ url('task/'.$tasks->id) }}" method="POST">
