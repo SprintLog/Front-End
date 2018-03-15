9a897929e27bbf75187bb1689ae92f356f4a3a7e
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-03-15 12:54:34 +0700

    Edit seeder for test  update

--- database/seeds/MatchSeeder.php
+++ database/seeds/MatchSeeder.php
@@ -24,54 +24,56 @@ class MatchSeeder extends Seeder
       $randomIndexUSer = rand(0, count($UserIDs));
       $randomIndexPJ = rand(0, count($ProjectIDs));
 
-      DB::table('matches')->insert([
-        "userId"    => 1,
-        "projectId" => 1,
-        'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-      ]);
-        for ($i=0; $i < 15; $i++) {
-          DB::table('matches')->insert([
-            "userId"    => $UserIDs[$i],
-            "projectId" => array_random($randomPj),
-            'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-          ]);
-        }
-        // DB::table('matches')->insert([
-        //   "userId"    => 1,
-        //   "projectId" => 1,
-        //   'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        //   'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        // ]);
-        //
-        // DB::table('matches')->insert([
-        //   "userId"    => 2,
-        //   "projectId" => 1,
-        //   'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        //   'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        // ]);
-        //
-        // DB::table('matches')->insert([
-        //   "userId"    => 3,
-        //   "projectId" => 1,
-        //   'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        //   'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        // ]);
-        //
-        // DB::table('matches')->insert([
-        //   "userId"    => 4,
-        //   "projectId" => 2,
-        //   'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        //   'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        // ]);
-        //
-        // DB::table('matches')->insert([
-        //   "userId"    => 4,
-        //   "projectId" => 3,
-        //   'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        //   'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
-        // ]);
+        // for ($i=0; $i < 15; $i++) {
+        //   DB::table('matches')->insert([
+        //     "userId"    => $UserIDs[$i],
+        //     "projectId" => array_random($randomPj),
+        //     'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        //     'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        //   ]);
+        // }
+        DB::table('matches')->insert([
+          "userId"    => 1,
+          "projectId" => 1,
+          'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('matches')->insert([
+          "userId"    => 2,
+          "projectId" => 1,
+          'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('matches')->insert([
+          "userId"    => 3,
+          "projectId" => 1,
+          'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('matches')->insert([
+          "userId"    => 4,
+          "projectId" => 2,
+          'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('matches')->insert([
+          "userId"    => 5,
+          "projectId" => 2,
+          'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('matches')->insert([
+          "userId"    => 6,
+          "projectId" => 2,
+          'created_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at'=> Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
 
     }
 }
