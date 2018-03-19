fad8cd4bf849b495c1b26733618fcfb7c07d2ed1
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-03-19 16:57:06 +0700

    :penguin: :green_apple: Bug btn minus

--- resources/views/projectInfoRegis.blade.php
+++ resources/views/projectInfoRegis.blade.php
@@ -19,33 +19,6 @@
   <script type="text/javascript">
   $("input").val()
   </script>
-  <script type="text/javascript">
-     $(document).ready(function(){
-      var next = 1;
-      $(".add-more").click(function(e){
-           e.preventDefault();
-           var addto = "#field" + next;
-           var addRemove = "#field" + (next);
-           next = next + 1;
-           var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
-           var newInput = $(newIn);
-           var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
-           var removeButton = $(removeBtn);
-           $(addto).after(newInput);
-           $(addRemove).after(removeButton);
-           $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-           $("#count").val(next);
-
-               $('.remove-me').click(function(e){
-                   e.preventDefault();
-                   var fieldNum = this.id.charAt(this.id.length-1);
-                   var fieldID = "#field" + fieldNum;
-                   $(this).remove();
-                   $(fieldID).remove();
-               });
-      });
-   });
-  </script>
 @endsection
 
 @section('content')
@@ -134,13 +107,15 @@
       <div class="col-sm-5">
          <div class="form-group">
             <div id="field">
-               <input autocomplete="off" class="input" id="field1" name="prof1" type="text"
+               <input   class="form-control" id="field1" name="prof1" type="text"
                 placeholder="Type something" data-items="8"/>
-                <button id="b1" class="btn add-more" type="button">
-                   +
-                </button>
              </div>
          </div>
+      </div>
+      <div class="col-sm-2">
+         <button id="b1" class="btn add-more" type="button">
+           +
+      </button>
       </div>
     </div>
 
@@ -174,12 +149,46 @@
 </div>
 <script type="text/javascript">
     var url = "{{ route('autocomplete.ajax') }}";
-    $('#search_text').typeahead({
+    $('#field1').typeahead({
         source:  function (query, process) {
         return $.get(url, { query: query }, function (data) {
                 return process(data);
           });
         }
     });
+</script>
+<script type="text/javascript">
+   $(document).ready(function(){
+    var next = 1;
+    $(".add-more").click(function(e){
+         e.preventDefault();
+         var addto = "#field" + next;
+         var addRemove = "#field" + (next);
+         next = next + 1;
+         var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
+         var newInput = $(newIn);
+         var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
+         var removeButton = $(removeBtn);
+         $(addto).after(newInput);
+         $(addRemove).after(removeButton);
+         $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+         $("#count").val(next);
+         $('.remove-me').click(function(e){
+           e.preventDefault();
+           var fieldNum = this.id.charAt(this.id.length-1);
+           var fieldID = "#field" + fieldNum;
+           $(this).remove();
+           $(fieldID).remove();
+         });
+         var url = "{{ route('autocomplete.ajax') }}";
+         $("#field" + next).typeahead({
+             source:  function (query, process) {
+             return $.get(url, { query: query }, function (data) {
+                     return process(data);
+               });
+             }
+         });
+    });
+ });
 </script>
 @endsection
