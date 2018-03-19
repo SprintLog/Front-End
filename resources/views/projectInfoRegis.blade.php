6a189a4918df78de0d7c777cbfc8c2ecc6e6984a
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-03-19 16:24:23 +0700

    :lipstick: :lipstick: Test Dynamic UI

--- resources/views/projectInfoRegis.blade.php
+++ resources/views/projectInfoRegis.blade.php
@@ -3,6 +3,14 @@
 @section('style')
   <link rel="stylesheet" href="{{elixir('css/bootstrap-tagsinput.css')}}">
   <link rel="stylesheet" href="{{elixir('css/fix-general.css')}}">
+  <style media="screen">
+   * {
+      .border-radius(0) !important;
+   }
+   #field {
+      margin-bottom:20px;
+   }
+  </style>
 @endsection
 
 @section('script')
@@ -11,7 +19,33 @@
   <script type="text/javascript">
   $("input").val()
   </script>
+  <script type="text/javascript">
+     $(document).ready(function(){
+      var next = 1;
+      $(".add-more").click(function(e){
+           e.preventDefault();
+           var addto = "#field" + next;
+           var addRemove = "#field" + (next);
+           next = next + 1;
+           var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
+           var newInput = $(newIn);
+           var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
+           var removeButton = $(removeBtn);
+           $(addto).after(newInput);
+           $(addRemove).after(removeButton);
+           $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+           $("#count").val(next);
 
+               $('.remove-me').click(function(e){
+                   e.preventDefault();
+                   var fieldNum = this.id.charAt(this.id.length-1);
+                   var fieldID = "#field" + fieldNum;
+                   $(this).remove();
+                   $(fieldID).remove();
+               });
+      });
+   });
+  </script>
 @endsection
 
 @section('content')
@@ -36,7 +70,7 @@
     </div>
   @endif
 <div class="jumbotron far">
-  <form class=""  action="{{ url('projectcreate') }}" method="post" enctype="multipart/form-data">
+  <form class="input-append"  action="{{ url('projectcreate') }}" method="post" enctype="multipart/form-data">
   {{ csrf_field() }}
     <div class="form-group row far">
       <label  class="col-sm-3 col-form-label">
@@ -61,7 +95,7 @@
           Type Project
       </label>
       <div class="col-sm-5">
-          <select class="form-control" name='type_project'>
+         <select class="form-control" name='type_project'>
           @foreach ($TypeProject as $t)
             <option value="{{$t->id}}">{{$t->type}}</option>
           @endforeach
@@ -89,6 +123,23 @@
       <div class="col-sm-5">
          <div class="form-group">
              <input class="form-control" type="text" id="search_text" name="search_text">
+         </div>
+      </div>
+    </div>
+
+    <div class="form-group row far">
+      <label  class="col-sm-3 col-form-label">
+          Developer
+      </label>
+      <div class="col-sm-5">
+         <div class="form-group">
+            <div id="field">
+               <input autocomplete="off" class="input" id="field1" name="prof1" type="text"
+                placeholder="Type something" data-items="8"/>
+                <button id="b1" class="btn add-more" type="button">
+                   +
+                </button>
+             </div>
          </div>
       </div>
     </div>
