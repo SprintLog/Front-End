12903fdf72e60ef0c36ecbef009037abdb790f11
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-03-24 12:07:31 +0700

    :heavy_plus_sign: :heavy_plus_sign: Change to Qury Builder from Equ :pencil2:

--- app/Http/Controllers/AutoComplateController.php
+++ app/Http/Controllers/AutoComplateController.php
@@ -3,19 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use Illuminate\Support\Facades\DB;
 class AutoComplateController extends Controller
 {
    public function ajaxDataStd(Request $request)
    {
       $query = $request->get('query','');
-      $users = User::where('name','LIKE','%'.$query.'%')->orWhere('typeUser',0)->get();
+      $users = DB::table('users')->where('name','LIKE','%'.$query.'%')->where('typeUser','=',0)->get();
+
       return response()->json($users);
    }
    public function ajaxDataLec(Request $request)
    {
       $query = $request->get('query','');
-      $users = User::where('name','LIKE','%'.$query.'%')->orWhere('typeUser',1)->get();
+      $users = DB::table('users')->where('name','LIKE','%'.$query.'%')->where('typeUser','=',1)->get();
       return response()->json($users);
    }
 }
