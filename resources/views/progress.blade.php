740e37f15a0a19f6a5e351cfc543d63cb7a063e9
Sarawut_064 <mamochiro11@gmail.com>
2018-04-17 23:42:14 +0700

    show progress task (Teacher)

---
+++ resources/views/progress.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.template')
+@section('style')
+
+@endsection
+
+@section('script')
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+@endsection
+
+@section('content')
+
+<style>
+.btn{
+
+  width: 100px;
+}
+</style>
+  <div class="jumbotron far">
+
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="/homeTeacher">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{{$projectName}}</li>
+      </ol>
+    </nav>
+
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Task</th>
+            <th scope="col">Date Submit</th>
+            <th scope="col">Status</th>
+            <th scope="col">CheckTask</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($tasks as $task)
+          <tr>
+            {{-- <th scope="column">3</th> --}}
+            <td><a href="/subTask/teacher/{{$task->id}}"><div>{{ $task->nametask }}</div></a></td>
+            {{-- <td>{{$task->nametask}}</td> --}}
+            <td>{{$task->created_at}}</td>
+            <td>
+
+              @for ($i=0; $i < sizeof($taskname); $i++)
+              @if ($task->nametask == $taskname[$i] && $task->id == $taskId[$i])
+                @if ($progressProject[$i] == 100 )
+                  <button type="button " name="button" class="btn btn-success">Complete</button>
+                  @if ($task->approved == 0 )
+                     <button type="button " name="button" class="btn btn-warning">Not approved</button>
+                  @else
+                     <button type="button " name="button" class="btn btn-success">approved</button>
+                  @endif
+                </td>
+                <td>
+                  <button type="button " name="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">Check</button>
+                </td>
+                </tr>
+                @else
+                  <button type="button " name="button" class="btn btn-warning">Waiting</button>
+                </td>
+                <td>
+                  <button type="button " name="button" class="btn btn-light" data-toggle="modal" data-target="#exampleModal1">Check</button>
+                </td>
+                </tr>
+                @endif
+              @endif
+            @endfor
+
+            {{-- </td>
+            <td><button type="button " name="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal1">Check</button>
+          </tr> --}}
+        @endforeach
+          </tbody>
+      </table>
+
+
+
+
+    </div>
+  </div>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            Do you want to approve this task?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger">Repair</button>
+          <button type="button" class="btn btn-success">Approve</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
