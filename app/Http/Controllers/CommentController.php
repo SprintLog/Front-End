e8e52535b2aedbf0df0944a14c35d18ecd796dd3
Sarawut_064 <mamochiro11@gmail.com>
2018-04-17 23:40:41 +0700

    Comment in subtasks view (Teacher)

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Comment;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+class CommentController extends Controller
+{
+    //
+
+    public function post(Request $request)
+    {
+
+      //dd($request);
+      $validator = Validator::make($request->all(), [
+          'body' => 'required|max:255',
+      ]);
+
+      if ($validator->fails()) {
+          return back()
+              ->withInput()
+              ->withErrors($validator)
+              ->with('warning', 'plz check input');
+      }
+
+      $comment = new Comment();
+      $comment->body = $request->body;
+      $comment->taskId = $request->taskId;
+      $comment->userId = Auth::user()->id;
+      $comment->save();
+      return back();
+    }
+}
---
+++ app/Http/Controllers/SubtaskTeacherController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Subtasks;
+use App\Project;
+use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+
+class SubtaskTeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $projectId =Task::find($id)->projectId;
+        $taskName =Task::find($id)->nametask;
+        $subtasks = DB::table('subtasks')->where('taskId' , $id)->get();
+        $images = DB::table('images')->where('taskId' , $id);
+        if ($images !== null) {
+          $images= $images->get();
+        }
+        $comments = DB::table('comment')
+            ->join('users', 'comment.userId', '=', 'users.id')
+            ->select('comment.*' ,'users.name' , 'users.lastname')
+            ->where('taskId', $id)
+            ->get();
+
+
+        return view('subtaskTeacher', ['taskId' => $id ,
+                                        'subtasks' => $subtasks,
+                                        'projectId' => $projectId ,
+                                         'taskName' => $taskName,
+                                         'images'   => $images,
+                                         'comments'  =>$comments
+                                       ]);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
