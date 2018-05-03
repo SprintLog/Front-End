a085f2426adc35db4ba87b2b398149ac80ba0917
Sutita_009 <s5730213009@phuket.psu.ac.th>
2018-05-03 10:25:45 +0700

    fix home for teacher

--- resources/views/homeTeacher.blade.php
+++ resources/views/homeTeacher.blade.php
@@ -2,29 +2,36 @@
 @section('style')
   <link rel="stylesheet" href="/css/EJ-kanban.css">
 @endsection
+
+@section('script')
+
+@endsection
+
 @section('content')
   <div class="jumbotron far">
     <div class="form-group row far">
-
       @foreach ($project as $project )
         <div class="col-sm-6 col-md-6">
-            <div class="thumbnail">
-                <div class="caption">
-                  <h3>{{$project->eng_name}} &nbsp; {{$project->thai_name}}</h3>
-                    <p>{{$project->abstack}}</p>
-                    <p>
+
+            <div class="panel panel-info">
+                   <div class="panel-heading ">
+                      {{$project->eng_name}}   &nbsp;({{$project->thai_name}})
+
+
+
                       <a href="{{ url('/projectTeacher', $project->id) }}"
-                        class="btn btn-primary btn-lg "
+                        class="btn btn-primary  pull-right "
                         role="button">View
                       </a> &nbsp;
+                   </div>
+                   <div class="panel-body">
+                       {{$project->abstack}}
+                   </div>
 
-
-
-                    </p>
                 </div>
-            </div>
-        </div>
+             </div>
       @endforeach
+
 
 
 @endsection
